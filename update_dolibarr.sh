c53305eaec59f0da698012b1e5518f6b2339a3d8
the PLATFORM CREATORS <133085287+RHEINMAINTECH@users.noreply.github.com>
2024-05-06 18:28:49 +0200

    Added missing backup of documents root
    Added missing backup of documents root dir (dolibarr_main_data_root)

--- update_dolibarr.sh
+++ update_dolibarr.sh
@@ -14,6 +14,7 @@ done < htdocs/conf/conf.php
 # Print Dolibarr configuration variables
 echo "Dolibarr main URL root: ${config[dolibarr_main_url_root]}"
 echo "Dolibarr main document root: ${config[dolibarr_main_document_root]}"
+echo "Dolibarr main data root: ${config[dolibarr_main_data_root]}"
 echo "Dolibarr main database host: ${config[dolibarr_main_db_host]}"
 echo "Dolibarr main database port: ${config[dolibarr_main_db_port]}"
 echo "Dolibarr main database name: ${config[dolibarr_main_db_name]}"
@@ -22,6 +23,7 @@ echo "Dolibarr main database password: ${config[dolibarr_main_db_pass]}"
 
 # Variables
 DOLIBARR_DIR="${config[dolibarr_main_document_root]}"
+DOLIBARR_DATA_DIR="${config[dolibarr_main_data_root]}"
 BACKUP_DIR="${DOLIBARR_DIR}_backup"
 LATEST_URL="https://api.github.com/repos/Dolibarr/dolibarr/releases/latest"
 #LATEST_URL="https://api.github.com/repos/Dolibarr/dolibarr/releases/tags/14.0.4"
@@ -33,6 +35,10 @@ mkdir -p ${BACKUP_DIR}
 echo "Creating a backup of the current Dolibarr installation..."
 backup_file="${BACKUP_DIR}/dolibarr_backup_$(date +%Y%m%d_%H%M%S).tar.gz"
 (cd "$(dirname "${DOLIBARR_DIR}")" && tar -czvf "${backup_file}" "$(basename "${DOLIBARR_DIR}")")
+
+echo "Creating a backup of the Documents Folder..."
+backup_file="${BACKUP_DIR}/dolibarr_backup_documents_$(date +%Y%m%d_%H%M%S).tar.gz"
+(cd "$(dirname "${DOLIBARR_DATA_DIR}")" && tar -czvf "${backup_file}" "$(basename "${DOLIBARR_DATA_DIR}")")
 
 echo "Backing up the Dolibarr database..."
 db_backup_file="${BACKUP_DIR}/dolibarr_db_backup_$(date +%Y%m%d_%H%M%S).sql.gz"
